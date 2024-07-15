d8a0650cebb201f7c0fa5807697a25f5ca08be15
Diego Villanueva <43555467+DiegoVilla27@users.noreply.github.com>
2024-07-15 02:16:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+type ViteConfig = {
+  mode: string;
+  command: string;
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
-});
+export default (args: ViteConfig) => {
+
+  const generateScopedName: string = (args.mode === 'production') ? '[hash:base64:8]' : '[local]_[hash:base64:8]'
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000
+    },
+    css: {
+      modules: {
+        scopeBehaviour: 'local',
+        generateScopedName,
+        localsConvention: 'camelCase'
+      }
+    }
+  })
+}
